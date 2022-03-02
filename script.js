4064977fa86c831f921c95987ce1c75f70d4dd12
amarmalakar <amarmalakarinfo@gmail.com>
2022-03-02 18:22:25 +0530

    first challange on frontend mentor

---
+++ script.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        difficulty: 'Newbie',
+        challanges: [
+            {
+                text: 'QR code component',
+                url: './qr-code-component-main/index.html'
+            }, 
+            // {
+            //     text: 'NFT preview card component',
+            //     url: './nft-preview-card-component-main/index.html'
+            // }, {
+            //     text: 'Order summary component challenge hub',
+            //     url: './order-summary-component-main/index.html'
+            // }, {
+            //     text: 'Stats Preview Card',
+            //     url: './stats-preview-card-component-main/index.html'
+            // }
+        ]
+    },
+    // {
+    //     difficulty: 'Junior',
+    //     challanges: [
+    //         {
+    //             text: 'Advice generator app',
+    //             url: '#'
+    //         }, {
+    //             text: 'Suite landing page',
+    //             url: '#'
+    //         }
+    //     ]
+    // }
+]
